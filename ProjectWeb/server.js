5e64a097d33c0d9d10e72988f6f4cd532e78970a
sierakiana <siera.camacho@colorado.edu>
2019-07-23 15:05:47 -0600

    unsuccessfully trying to navigate from poet info to any other page

--- ProjectWeb/server.js
+++ ProjectWeb/server.js
@@ -29,7 +29,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 //This line is necessary for us to use relative paths and access our resources directory
 
-// registration page
 app.get('/poetinfo', function(req, res) {
   var query = 'select * from poet_info;';
   db.any(query)
